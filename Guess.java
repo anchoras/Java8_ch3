dcbfb732876dd9ae61fcbe5f423e40b8df95a6e4
anchoras <anchoras2@gmail.com>
2020-12-24 23:02:45 +0300

    added do-while (until get right unswer)

--- Guess.java
+++ Guess.java
@@ -6,22 +6,30 @@
 class Guess {
     public static void main (String args[])
         throws java.io.IOException {
-            char ch, answer = 'S';
+            char ch, ignore, answer = 'S';
             System.out.println("The letter belongs to A-Z");
-            System.out.println("Let's guess which letter is the answer");
+            do {
+                System.out.println("Let's guess which letter is the answer");
 
-            ch = (char) System.in.read();
-            if (ch == answer) {
-                System.out.println("** You are right! **");
-            }
-            else {
-                System.out.println("Sorry, you was wrong");
+                ch = (char) System.in.read();
+                do {
+                    ignore = (char) System.in.read();
+                } while (ignore != '\n');
+
+                if (ch == answer) {
+                    System.out.println("** You are right! **");
+                }
+                else {
+                    System.out.println("Sorry, you was wrong");
                 if (ch < answer) {
                     System.out.println("The letter is closer to alphabet's end");
                 }
                 else {
                     System.out.println("The letter is closer to alphabet's begin");
                 }
+                System.out.print("\n\n");
             }
+            } while (ch != answer);
+            
         }
 }
